abf1fd2b92e0dfda0dc0313a38a1c7c071cebf4e
Spoutnique <spootnique@gmail.com>
2016-03-16 16:43:28 +0100

    commitFocusSceneDone

--- VR/Assets/Prototypes/Scripts/Mohamed/ReticulesPlacement.cs
+++ VR/Assets/Prototypes/Scripts/Mohamed/ReticulesPlacement.cs
@@ -6,7 +6,8 @@ public class ReticulesPlacement : MonoBehaviour {
 	public Image ReticuleL,ReticuleR;
 
 	void Start () {
-	
+		//ReticuleL.rectTransform.anchoredPosition = new Vector2(0.25f,0.5f);
+		//ReticuleR.rectTransform.anchoredPosition = new Vector2(0.75f,0.5f);
 	}
 	
 
--- VR/Assets/Prototypes/Scripts/Mohamed/StareDetection.cs
+++ VR/Assets/Prototypes/Scripts/Mohamed/StareDetection.cs
@@ -9,7 +9,7 @@ public class StareDetection : MonoBehaviour {
 	private Vector3 currentDestination,newDestination; 
 	public Image LoadingCursorL,LoadingCursorR;
 	private int currentIndex;
-	//private bool selectedTaken;
+	private bool selectedTaken;//selectedTaken is true during travels, remove it causes u to stop mid travel
 
 	void Start () {
 		coolDown = 2.0f;
@@ -21,13 +21,13 @@ public class StareDetection : MonoBehaviour {
 	void Update () {
 		
 		if (Physics.Raycast(transform.position,transform.forward,out hit, 10.0f)) {
-			if ((hit.transform.name == "Destination0") /* && (!selectedTaken)*/)  {
+			if ((hit.transform.name == "Destination0")  && (!selectedTaken))  {
 				interactibleObjects[0].GetComponent<InteractibleObject>().selected = true; Camera.main.WorldToScreenPoint(hit.point); // use hit.point to get the reticule pos set it with code
 				} 
-			else if ((hit.transform.name == "Destination1") /*&& (!selectedTaken)*/) {
+			else if ((hit.transform.name == "Destination1") && (!selectedTaken)) {
 				interactibleObjects[1].GetComponent<InteractibleObject>().selected = true;
 				}
-			else if ((hit.transform.name == "Destination2") /* && (!selectedTaken)*/)  { 
+			else if ((hit.transform.name == "Destination2")  && (!selectedTaken))  { 
 				interactibleObjects[2].GetComponent<InteractibleObject>().selected = true;
 				}
 
@@ -35,7 +35,7 @@ public class StareDetection : MonoBehaviour {
 
 				} else { //this else means the ray you're casting got out
 			for (int i = 0; (interactibleObjects[i] != null) && (i < interactibleObjects.Length); i++)	{
-				interactibleObjects[i].GetComponent<InteractibleObject>().selected = false;}//notice : make sure the value of selected stays for the travel
+			if (timer < coolDown) interactibleObjects[i].GetComponent<InteractibleObject>().selected = false;}//remove the "if" 2 b able to stop mid travel
 			if (timer < coolDown) {timer = 0.0f; LoadingCursorL.fillAmount = 0;LoadingCursorR.fillAmount = 0;}
 				
 				}
@@ -47,21 +47,22 @@ public class StareDetection : MonoBehaviour {
 			CoolDown(interactibleObjects[i]); currentIndex = i;}
 		 }
 
-		Debug.Log(interactibleObjects[currentIndex].GetComponent<InteractibleObject>().selected);
+
 		//if (selectedTaken) interactibleObjects[currentIndex].GetComponent<InteractibleObject>().selected = true;
-	
+		Debug.Log(interactibleObjects[currentIndex].GetComponent<InteractibleObject>().selected);
+
 	}//closing Update()
 
 	public void GoToIO (Vector3 Destination) {
-		transform.parent.transform.position = Vector3.MoveTowards(transform.parent.transform.position,Destination, 2*Time.deltaTime);
-		if (transform.parent.transform.position == Destination) {timer = 0; LoadingCursorL.fillAmount = 0;LoadingCursorR.fillAmount = 0;/*selectedTaken = false;*/}
+		transform.parent.transform.position = Vector3.MoveTowards(transform.parent.transform.position,Destination, 3*Time.deltaTime);
+		if (transform.parent.transform.position == Destination) {timer = 0; LoadingCursorL.fillAmount = 0;LoadingCursorR.fillAmount = 0;selectedTaken = false;}
 	}
 
 	public void CoolDown (GameObject theInteractibleObject) {
 		if ((timer < coolDown) && (Vector3.Distance(transform.parent.transform.position,theInteractibleObject.transform.position) > 2)) {
 			timer += Time.deltaTime;
 			LoadingCursorL.fillAmount += 0.5f * Time.deltaTime;LoadingCursorR.fillAmount += 0.5f * Time.deltaTime;
-		} else {//selectedTaken = true;
+		} else {selectedTaken = true;
 			newDestination = new Vector3(theInteractibleObject.transform.position.x, theInteractibleObject.transform.position.y, theInteractibleObject.transform.position.z-2);
 			GoToIO(newDestination);
 			//StartCoroutine(ResetTimer());
