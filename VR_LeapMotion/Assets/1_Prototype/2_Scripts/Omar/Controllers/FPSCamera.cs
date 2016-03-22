f6ed8aeda5cdb594412c4349716d60034663ced8
louhichio <louhichio@hotmail.com>
2016-03-22 17:37:51 +0100

    Standard Assets effects

---
+++ VR_LeapMotion/Assets/1_Prototype/2_Scripts/Omar/Controllers/FPSCamera.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Quinq
+{
+	public class FPSCamera : Singleton<FPSCamera> 
+	{
+		public Transform player;
+		public float height;
+
+		void FixedUpdate () 
+		{
+			if(player)
+			{
+				this.transform.transform.position = player.transform.position + Vector3.up * height;
+			}
+		}
+	}
+}
---
+++ VR_LeapMotion/Assets/1_Prototype/2_Scripts/Omar/Controllers/FPSInput.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Quinq
+{
+	public class FPSInput : Singleton<FPSInput> 
+	{
+		void OnGUI()
+		{
+			float h = Screen.height / 2;
+			GUI.Label (new Rect (0, h, 100, 100), Input.GetAxis("Horizontal").ToString());
+			GUI.Label (new Rect (0, h + 25, 100, 100), Input.GetAxis("Vertical").ToString());
+		}	
+	}
+}
